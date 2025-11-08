72296467066f08911152ba1192fde6a5ef8ce585
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-11-08 20:31:54 +0800

    feat: breakThisPage

--- assets/ts/SunsetBlog.mjs
+++ assets/ts/SunsetBlog.mjs
@@ -2,6 +2,7 @@ import Confetti from "./confetti.mjs";
 import * as tools from "./tools.mjs";
 // import jQuery from "./jquery-3.7.1.js";
 import Cookies from "./js.cookie.mjs";
+import glitch from "./glitch.mjs";
 
 export default function () {
     if (!window) {
@@ -11,25 +12,34 @@ export default function () {
 
     var SunsetBlog = {};
 
+    // ping
     SunsetBlog.ping = function () {
         return "pong";
     };
 
+    // ondemand css and js loader
     SunsetBlog.loadCSSPreload = tools.loadCSSPreload;
     SunsetBlog.addScriptTag = tools.addScriptTag;
 
+    // confetti animation
     SunsetBlog.Confetti = Confetti;
 
+    // jump to debug
     SunsetBlog.goDebug = function () {
         window.location.href = "/debug";
     };
 
+    // detect root domain
     SunsetBlog.detectRootDomainByCookieTest =
         tools.detectRootDomainByCookieTest;
 
     // SunsetBlog.jQuery = jQuery;
 
+    // cookies api
     SunsetBlog.Cookies = Cookies;
+
+    // page glitch
+    SunsetBlog.breakThisPage = glitch;
 
     window.SunsetBlog = SunsetBlog;
 }
