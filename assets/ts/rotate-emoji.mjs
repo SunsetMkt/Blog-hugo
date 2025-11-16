3e11cc184f5891a5a8a5dd4a0ad65160d5ff9fd8
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-11-16 12:19:24 +0800

    feat: emoji preload

--- assets/ts/rotate-emoji.mjs
+++ assets/ts/rotate-emoji.mjs
@@ -1,24 +1,39 @@
+// https://stackoverflow.com/a/78881602
+export function countryCodeToFlag(countryCode) {
+    // Validate the input to be exactly two characters long and all alphabetic
+    if (
+        !countryCode ||
+        countryCode.length !== 2 ||
+        !/^[a-zA-Z]+$/.test(countryCode)
+    ) {
+        return "🏳️"; // White Flag Emoji for unknown or invalid country codes
+    }
+
+    // Convert the country code to uppercase to match the regional indicator symbols
+    const code = countryCode.toUpperCase();
+
+    // Calculate the offset for the regional indicator symbols
+    const offset = 127397;
+
+    // Convert each letter in the country code to its corresponding regional indicator symbol
+    const flag = Array.from(code)
+        .map((letter) => String.fromCodePoint(letter.charCodeAt(0) + offset))
+        .join("");
+
+    return flag;
+}
+
 export default function () {
     // Change emoji on click
     var emojiSpan = document.getElementsByClassName("emoji")[0];
-    var emojiList = [
-        "🇺🇦",
-        "🏳️‍🌈",
-        "🏳️‍⚧️",
-        "🦄",
-        "🏴‍☠️",
-        "🍥",
-        "🐍",
-        "🐦‍🔥",
-        "🐉",
-        "🦖",
-        "🐧",
-        "🇺🇳",
-        "🇦🇶",
-        "💻",
-        "⌨️",
-        "📎",
-    ];
+    var emojiList = ["🇺🇦", "🏳️‍🌈", "🏳️‍⚧️", "🦄", "🇺🇳", "📎"];
+
+    // Add current user country flag to emoji list
+    var countryCode = navigator.language.split("-")[1];
+    if (countryCode) {
+        emojiList.push(countryCodeToFlag(countryCode));
+    }
+
     var currentIndex = 0;
 
     // Set the initial emoji
@@ -43,40 +58,14 @@ export default function () {
     // Prevent text selection
     // emojiSpan.style.userSelect = "none";
 
-    // https://stackoverflow.com/a/78881602
-    function countryCodeToFlag(countryCode) {
-        // Validate the input to be exactly two characters long and all alphabetic
-        if (
-            !countryCode ||
-            countryCode.length !== 2 ||
-            !/^[a-zA-Z]+$/.test(countryCode)
-        ) {
-            return "🏳️"; // White Flag Emoji for unknown or invalid country codes
-        }
-
-        // Convert the country code to uppercase to match the regional indicator symbols
-        const code = countryCode.toUpperCase();
-
-        // Calculate the offset for the regional indicator symbols
-        const offset = 127397;
-
-        // Convert each letter in the country code to its corresponding regional indicator symbol
-        const flag = Array.from(code)
-            .map((letter) =>
-                String.fromCodePoint(letter.charCodeAt(0) + offset),
-            )
-            .join("");
-
-        return flag;
-    }
-
-    // Add current user country flag to emoji list and allow it to be displayed on next click
-    var countryCode = navigator.language.split("-")[1];
-    emojiList.unshift(countryCodeToFlag(countryCode));
-    currentIndex = -1;
-    // rotateEmoji(emojiSpan);
+    var emojiListPlain = emojiList.join("");
+    requestIdleCallback(() => {
+        document.fonts.load('40px "Noto Color Emoji"', emojiListPlain);
+    });
 
     if (window.SunsetBlog) {
         window.SunsetBlog.emojiSpan = emojiSpan;
+        window.SunsetBlog.emojiList = emojiList;
+        window.SunsetBlog.rotateEmoji = rotateEmoji;
     }
 }
