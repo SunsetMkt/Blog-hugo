080a294195fd395b52ad9bde4ecdd8c9a2cba131
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-11-17 22:20:35 +0800

    docs: animateCSS

--- assets/ts/tools.mjs
+++ assets/ts/tools.mjs
@@ -81,12 +81,14 @@ export async function loadCSSPreload(href) {
 
 // animate.css
 export function animateCSS(element, animation, prefix = "animate__") {
+    // We create a Promise and return it
     return new Promise((resolve, reject) => {
         const animationName = `${prefix}${animation}`;
         const node = document.querySelector(element);
 
         node.classList.add(`${prefix}animated`, animationName);
 
+        // When the animation ends, we clean the classes and resolve the Promise
         function handleAnimationEnd(event) {
             event.stopPropagation();
             node.classList.remove(`${prefix}animated`, animationName);
