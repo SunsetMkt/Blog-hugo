6821827e83e7e17b3e4028aa79f2a1d05138785b
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-12-08 12:39:41 +0800

    feat: executeImmediately

--- assets/ts/interval-and-event.mjs
+++ assets/ts/interval-and-event.mjs
@@ -15,7 +15,10 @@ function safeRun(func) {
     }
 }
 
-async function onLoadExecute() {
+function executeImmediately() {
+    // The whole script is put at bottom, so no need
+    // to worry about missing elements
+
     // waline-statistic flag
     safeRun(function () {
         var w = document.getElementById("waline-statistic");
@@ -23,34 +26,14 @@ async function onLoadExecute() {
             w.style.removeProperty("display");
         }
     });
-    // save-ukraine flag
-    safeRun(function () {
-        if (featureFlags.isFlagSet("save-ukraine")) {
-            tools.addScriptTag(
-                "https://unpkg.com/save-ukraine@0.18.140/dist/umd/main.js",
-                true,
-                function () {
-                    // eslint-disable-next-line no-undef
-                    Ukraine.save({
-                        ribbon: "TOP_LEFT",
-                        countries: [],
-                        hasShadow: true,
-                        isCancelable: true,
-                        isBloodIncluded: false,
-                        isGraphicIncluded: false,
-                        isInConsole: false,
-                        moreInfoUrl: "https://war.ukraine.ua/",
-                    });
-                },
-            );
-        }
-    });
+
     // enforce-grayscale flag
     safeRun(function () {
         if (featureFlags.isFlagSet("enforce-grayscale")) {
             grayscale.addGrayscale(document.body);
         }
     });
+
     // outlink-alert flag
     safeRun(function () {
         if (featureFlags.isFlagSet("outlink-alert")) {
@@ -105,6 +88,34 @@ async function onLoadExecute() {
             });
         }
     });
+}
+
+async function onLoadExecute() {
+    // Put blocking or time consuming code here
+
+    // save-ukraine flag
+    safeRun(function () {
+        if (featureFlags.isFlagSet("save-ukraine")) {
+            tools.addScriptTag(
+                "https://unpkg.com/save-ukraine@0.18.140/dist/umd/main.js",
+                true,
+                function () {
+                    // eslint-disable-next-line no-undef
+                    Ukraine.save({
+                        ribbon: "TOP_LEFT",
+                        countries: [],
+                        hasShadow: true,
+                        isCancelable: true,
+                        isBloodIncluded: false,
+                        isGraphicIncluded: false,
+                        isInConsole: false,
+                        moreInfoUrl: "https://war.ukraine.ua/",
+                    });
+                },
+            );
+        }
+    });
+
     // back-to-top flag
     safeRun(function () {
         if (featureFlags.isFlagSet("back-to-top")) {
@@ -117,6 +128,7 @@ async function onLoadExecute() {
 }
 
 export default function () {
+    executeImmediately();
     if (document.readyState === "loading") {
         document.addEventListener("DOMContentLoaded", onLoadExecute);
     } else onLoadExecute();
