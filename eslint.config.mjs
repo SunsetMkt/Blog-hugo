5ac4af8b5c177b53749fdc2a27c82a34301b354d
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2026-02-24 21:24:28 +0800

    chore: eslintConfigPrettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,10 @@ import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
+// Note the `/flat` suffix here, the difference from default entry is that
+// `/flat` added `name` property to the exported object to improve
+// [config-inspector](https://eslint.org/blog/2024/04/eslint-config-inspector/) experience.
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
     { ignores: ["node_modules/", "public/", "resources/", "static/"] },
@@ -15,34 +19,35 @@ export default defineConfig([
         languageOptions: { globals: globals.browser },
     },
     tseslint.configs.recommended,
-    // {
-    //     files: ["**/*.json"],
-    //     plugins: { json },
-    //     language: "json/json",
-    //     extends: ["json/recommended"],
-    // },
-    // {
-    //     files: ["**/*.jsonc"],
-    //     plugins: { json },
-    //     language: "json/jsonc",
-    //     extends: ["json/recommended"],
-    // },
-    // {
-    //     files: ["**/*.json5"],
-    //     plugins: { json },
-    //     language: "json/json5",
-    //     extends: ["json/recommended"],
-    // },
-    // {
-    //     files: ["**/*.md"],
-    //     plugins: { markdown },
-    //     language: "markdown/commonmark",
-    //     extends: ["markdown/recommended"],
-    // },
-    // {
-    //     files: ["**/*.css"],
-    //     plugins: { css },
-    //     language: "css/css",
-    //     extends: ["css/recommended"],
-    // },
+    {
+        files: ["**/*.json"],
+        plugins: { json },
+        language: "json/json",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.jsonc"],
+        plugins: { json },
+        language: "json/jsonc",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.json5"],
+        plugins: { json },
+        language: "json/json5",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.md"],
+        plugins: { markdown },
+        language: "markdown/commonmark",
+        extends: ["markdown/recommended"],
+    },
+    {
+        files: ["**/*.css"],
+        plugins: { css },
+        language: "css/css",
+        extends: ["css/recommended"],
+    },
+    eslintConfigPrettier,
 ]);
